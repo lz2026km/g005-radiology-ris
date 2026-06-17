6da6c3d39b55d2cf079db4514b637384d1018678
G005 Dev <dev@g005.ris>
2026-06-17 19:19:37 +0800

    chore: remove temp scripts

--- check_en.py
+++
@@ -1,6 +0,0 @@
-import json
-for fname in ['zh_CN.json', 'en_US.json']:
-    with open(f'src/i18n/locales/{fname}', 'r', encoding='utf-8') as f:
-        data = json.load(f)
-    val = data.get('nav.revenue', 'MISSING')
-    print(f'{fname} nav.revenue: {val}')
